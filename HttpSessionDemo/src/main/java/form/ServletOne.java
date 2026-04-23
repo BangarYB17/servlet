750b5c255a5bb512a7413496c53029ff825e0714
BangarYB17 <bangaryogeshwar3@gmail.com>
2026-04-23 14:57:05 +0530

    Session Tracking Mechanism:4)Http Session

---
+++ HttpSessionDemo/src/main/java/form/ServletOne.java
@@ -0,0 +1,56 @@
+package form;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class ServletOne
+ */
+@WebServlet("/ServletOne")
+public class ServletOne extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletOne() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			response.setContentType("text/html");
+			PrintWriter pw = response.getWriter();
+			
+			String nm = request.getParameter("username");
+			pw.print("Welcome "+nm);
+			pw.print("<br>");
+			HttpSession hs = request.getSession();
+			hs.setAttribute("sname", nm);
+			
+			pw.print("<a href='ServletTwo'>Visit</a>");
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
